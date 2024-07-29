f84c6c40e813ac4b1dcf3b3bb54d3186f33d2010
IZUMI-Zu <274620705z@gmail.com>
2024-07-29 20:50:52 +0800

    chore: remove empty JSX element

--- App.js
+++ App.js
@@ -32,6 +32,7 @@ const App = () => {
         <NavigationContainer>
           <PaperProvider>
             <Header />
+            <></>
             <NavigationBar />
           </PaperProvider>
         </NavigationContainer>
